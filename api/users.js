e672885df5a40c2b9a8d22514c110375b310b8a3
LGarzaN <luis.garzanaranjo@gmail.com>
2024-02-02 17:27:39 -0600

    commit

--- api/users.js
+++ api/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     res.status(200).json({
         message: "users"
-    });
+    }); 
 });
 
 router.post("/", (req, res, next) => {
