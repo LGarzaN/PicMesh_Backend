6ab9b6b1c6390cc45da69910bf605a0b1cdc58c2
LGarzaN <luis.garzanaranjo@gmail.com>
2024-02-02 17:24:53 -0600

    Initial Commit

---
+++ api/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.status(200).json({
+        message: "users"
+    });
+});
+
+router.post("/", (req, res, next) => {
+    const user = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: "users",
+        createdUser: user
+    });
+});
+
+module.exports = router;
