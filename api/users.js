09f64a56e54591d3ebcad32e65242155e5ac4f41
LGarzaN <luis.garzanaranjo@gmail.com>
2024-02-19 00:01:02 -0600

    username added to jwt

--- api/users.js
+++ api/users.js
@@ -160,6 +160,7 @@ router.post("/login", (req, res, next) => {
                     const token = jwt.sign({
                         phone: results[0].Phone,
                         userId: results[0].UserId,
+                        username: results[0].UserName,
                     }, process.env.JWT_KEY, {});
                     return res.status(200).json({
                         message: "Auth successful",
